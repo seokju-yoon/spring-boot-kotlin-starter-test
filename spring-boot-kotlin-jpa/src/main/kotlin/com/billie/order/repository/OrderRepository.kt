e4cfa1188ac136931c85c88760430d309f3e6a85
윤석주 <monster1354@gentlemonster.com>
2025-02-05 15:20:05 +0900

    update jpa build.gradle.kts

--- spring-boot-kotlin-jpa/src/main/kotlin/com/billie/order/repository/OrderRepository.kt
+++ spring-boot-kotlin-jpa/src/main/kotlin/com/billie/order/repository/OrderRepository.kt
@@ -3,5 +3,5 @@ package com.billie.order.repository
 import com.billie.order.entity.Order
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface OrderRepository : JpaRepository<Order, Long>, OrderRepositoryCustom {
+interface OrderRepository : JpaRepository<Order, Long> {
 }
